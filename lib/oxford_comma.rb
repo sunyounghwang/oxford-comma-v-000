a826987780dad495d3199fc2e2e6569d181ce84e
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-08 01:31:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
   if array.size == 2
     array.join(" and ")
   elsif array.size >= 3
-    last_element = array.pop
-    str = array.join(", ")
-    str + ", and #{last_element}"
+    array.insert(0, " and ")
   end
   array.join(", ")
 end
