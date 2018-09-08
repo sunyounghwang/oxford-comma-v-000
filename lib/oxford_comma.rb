c753ac801cc6d4d966ee267dc5434160d51c945b
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-08 01:29:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
-  if array.size == 1
-    array.join
-  elsif array.size == 2
+  if array.size == 2
     array.join(" and ")
   elsif array.size >= 3
     last_element = array.pop
     str = array.join(", ")
     str + ", and #{last_element}"
   end
+  array.join(", ")
 end
