f0961a18bb1bfde26d77f4755b2d36f9dabf3b1a
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-08 01:34:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.size == 2
-    "#{array[0]} and #{array[1]}"
+    return "#{array[0]} and #{array[1]}"
   elsif array.size >= 3
     array[-1].insert(0, "and ")
   end
